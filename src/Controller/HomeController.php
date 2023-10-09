88ae5d84e46ecbd80cffbc24f7122747e45718b6
x0-3 <steph6362@gmail.com>
2023-10-09 22:07:51 +0200

    homepage still need user img and links

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Topic;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+
+        $topics = $em->getRepository(Topic::class)->findBy([], ['createdAt' => 'DESC'], 5);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'topics' => $topics,
             'description' => 'HomeController'
         ]);
     }
