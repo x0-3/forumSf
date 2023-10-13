faaaaaddf553fce5f82fd7fb20bfc323f92a687c
x0-3 <steph6362@gmail.com>
2023-10-13 20:26:05 +0200

    detail for a user / link the path to the profile page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,6 +24,7 @@ class UserController extends AbstractController
     public function profile(EntityManagerInterface $em): Response
     {
 
+        // get user 
         $user = $this->getUser();
 
         if($user instanceof User) {
@@ -32,13 +33,37 @@ class UserController extends AbstractController
                 return $this->redirectToRoute('app_home');
             }
 
+            // find the topics created by the user that is logged in
             $topics = $em->getRepository(Topic::class)->findByUser($user->getId(), 5);
         
             return $this->render('user/profile.html.twig', [
                 'topics' => $topics,
-                'description' => 'profile page'
+                'description' => $user->getUsername() . 'profile page'
             ]);
         }
+
+    }
+
+
+    #[Route('/user/{id}', name: 'app_user')]
+    public function detailUser(User $user, EntityManagerInterface $em): Response
+    {
+
+        $loggedUser = $this->getUser(); // get the user currently logged in
+
+        // if the logged in user is the same as the user in the user details page
+        if ($loggedUser == $user) {
+            return $this->redirectToRoute('app_profile');
+        }
+
+        // find the topics created by the user that is logged in
+        $topics = $em->getRepository(Topic::class)->findByUser($user->getId(), 5);
+
+        return $this->render('user/detailUser.html.twig', [
+            'user' => $user,
+            'topics' => $topics,
+            'description' => $user->getUsername() . 'page'
+        ]);
 
     }
 }
