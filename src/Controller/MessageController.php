4a69b6549911a04bb6909f5f5e5215f696f49920
x0-3 <steph6362@gmail.com>
2023-10-13 21:49:13 +0200

    add a message

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Topic;
+use App\Entity\Message;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MessageController extends AbstractController
+{
+    #[Route('/message', name: 'app_message')]
+    public function index(): Response
+    {
+        return $this->render('message/index.html.twig', [
+            'controller_name' => 'MessageController',
+        ]);
+    }
+
+    #[Route('/{topicId}/newMessage', name: 'new_message')]
+    public function newMessage(Request $request, EntityManagerInterface $em, Topic $topicId): Response
+    {
+
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('detail_topic', ['id' => $topicId->getId()]);
+        }
+
+        $message = new Message();
+
+        $form = $this->createForm(MessageType::class, $message);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $form->getData();
+
+            $message->setCreatedAt(new \DateTime());
+            $message->setUser($user);
+            $message->setTopic($topicId);
+
+            $em->persist($message);
+
+            $em->flush();
+
+            return $this->redirectToRoute('detail_topic', ['id' => $topicId->getId()]);
+        }
+
+        return $this->render('message/newMessage.html.twig', [
+            'form' => $form,
+            'description' => 'create a new message',
+        ]);
+
+    }
+}
---
+++ src/Form/MessageType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
