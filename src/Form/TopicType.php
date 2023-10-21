2e259662eb8ff50b634bb4adab22e6da61258cdb
x0-3 <steph6362@gmail.com>
2023-10-21 21:14:09 +0200

    texttype change to text area type

--- src/Form/TopicType.php
+++ src/Form/TopicType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\Topic;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +14,7 @@ class TopicType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
+            ->add('title', TextareaType::class)
             ->add('submit', SubmitType::class)
             // ->add('islocked')
         ;
